3cff1ab6b775c5e14894a740bf6564ec78e62924
Jesus Gonzalez <jesusgonzr@gmail.com>
2023-06-14 16:17:27 +0200

    Implementación del commando GetWinnerContractHandler

---
+++ LobbyWars.Application/Commands/Extensions/GetWinnerContractExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LobbyWars.Domain.Contracts;
+
+namespace LobbyWars.Application.Commands.Extensions
+{
+    /// <summary>
+    /// GetWinnerContractExtensions class.
+    /// </summary>
+    public static class GetWinnerContractExtensions
+    {
+        /// <summary>
+        /// Chech commmand value.
+        /// </summary>
+        /// <param name="request">GetWinnerContract object.</param>
+        public static void CheckCommandRequest(this GetWinnerContract request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            if (string.IsNullOrEmpty(request.Contract1))
+            {
+                throw new ArgumentNullException(nameof(request.Contract1));
+            }
+
+            if (string.IsNullOrEmpty(request.Contract2))
+            {
+                throw new ArgumentNullException(nameof(request.Contract2));
+            }
+        }
+
+        /// <summary>
+        /// Check if the contracts are the same.
+        /// </summary>
+        /// <param name="request">>GetWinnerContract object.</param>
+        /// <returns>True or false.</returns>
+        public static bool IsSameContracts(this GetWinnerContract request)
+        {
+            return request.Contract1 == request.Contract2;
+        }
+    }
+}
--- LobbyWars.Application/Commands/GetWinnerContractHandler.cs
+++ LobbyWars.Application/Commands/GetWinnerContractHandler.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using LobbyWars.Application.Commands.Extensions;
+using LobbyWars.Application.Interfaces;
+using LobbyWars.Domain.Contracts;
 using MediatR;
 
 namespace LobbyWars.Application.Commands
@@ -12,10 +16,60 @@ namespace LobbyWars.Application.Commands
     /// </summary>
     public class GetWinnerContractHandler : IRequestHandler<GetWinnerContract, string>
     {
+        private readonly IWinnerContractQuery queryRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetWinnerContractHandler"/> class.
+        /// </summary>
+        /// <param name="query">IWinnerContractQuery object.</param>
+        public GetWinnerContractHandler(IWinnerContractQuery query)
+        {
+            this.queryRepository = query ?? throw new ArgumentNullException(nameof(query));
+        }
+
         /// <inheritdoc/>
         public Task<string> Handle(GetWinnerContract request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            // Check request value.
+            request.CheckCommandRequest();
+
+            // Return results.
+            return Task.Run(() =>
+            {
+                return this.GetResult(request);
+            });
+        }
+
+        /// <summary>
+        /// Compare contracts and return the result.
+        /// </summary>
+        /// <param name="request">GetWinnerContract object.</param>
+        /// <returns>Contract winner.</returns>
+        private string GetResult(GetWinnerContract request)
+        {
+            // Check if the contracts are the same.
+            if (request.IsSameContracts())
+            {
+                return string.Empty;
+            }
+
+            // Get contract objects.
+            var contract1 = new Contract(request.Contract1);
+            var contract2 = new Contract(request.Contract2);
+
+            // Get points of each contract.
+            int contract1Point = this.queryRepository.GetPoints(contract1);
+            int contract2Point = this.queryRepository.GetPoints(contract2);
+
+            // Compare and return the result.
+            if (contract1Point > contract2Point)
+            {
+                return request.Contract1;
+            }
+            else
+            {
+                return request.Contract2;
+            }
         }
     }
 }
