ae0f105c597df8a9a30e0ef75e1f3d7fb1291881
Jesus Gonzalez <jesusgonzr@gmail.com>
2023-06-14 14:58:38 +0200

    Se agrega interfaz de consulta para comprobar los puntos de cada contrato y su implementación de la clase

---
+++ LobbyWars.Application/Interfaces/IWinnerContractQuery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LobbyWars.Domain.Contracts;
+
+namespace LobbyWars.Application.Interfaces
+{
+    /// <summary>
+    /// IWinnerContractQuery interface.
+    /// </summary>
+    public interface IWinnerContractQuery
+    {
+        /// <summary>
+        /// Get contract points.
+        /// </summary>
+        /// <param name="contract">Contract object.</param>
+        /// <returns>Points.</returns>
+        int GetPoints(Contract contract);
+    }
+}
---
+++ LobbyWars.Application/Queries/WinnerContractQuery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LobbyWars.Application.Interfaces;
+using LobbyWars.Domain.Contracts;
+
+namespace LobbyWars.Application.Queries
+{
+    /// <summary>
+    /// WinnerContractQuery class.
+    /// </summary>
+    public class WinnerContractQuery : IWinnerContractQuery
+    {
+        /// <inheritdoc/>
+        public int GetPoints(Contract contract)
+        {
+            if (contract.ContainsRoleKing())
+            {
+                return contract.GetSignatures.Where(c => c.GetSignatureRoleValue != SignatureRole.Validator).Select(s => s.GetRoleValue).Sum();
+            }
+            else
+            {
+                return contract.GetSignatures.Select(s => s.GetRoleValue).Sum();
+            }
+        }
+    }
+}
