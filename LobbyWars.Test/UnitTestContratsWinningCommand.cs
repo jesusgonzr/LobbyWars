05332c4af9b50af6e4abdc0729fd042acaa9ef7c
Jesus Gonzalez <jesusgonzr@gmail.com>
2023-06-14 11:59:50 +0200

    Agrego nuevo metodo en el test unitario del ganador de contratos.

--- LobbyWars.Test/UnitTestContratsWinningCommand.cs
+++ LobbyWars.Test/UnitTestContratsWinningCommand.cs
@@ -78,5 +78,31 @@ namespace LobbyWars.Test
             Assert.True(result == contract2);
             this.output.WriteLine(result);
         }
+
+        [Fact]
+        public void ContractsIsSame()
+        {
+            var mediatorMock = new Mock<IMediator>();
+            var commandHandler = new GetWinnerContractHandler();
+
+            mediatorMock.Setup(m => m.Send(It.IsAny<GetWinnerContract>(), It.IsAny<CancellationToken>()))
+                        .Returns<GetWinnerContract, CancellationToken>(async (command, token) => await commandHandler.Handle(command, token));
+
+            string contract1 = "NNV";
+            string contract2 = "NNV";
+            var command = new GetWinnerContract()
+            {
+                Contract1 = contract1,
+                Contract2 = contract2,
+            };
+            this.output.WriteLine(JsonConvert.SerializeObject(command));
+
+            var result = mediatorMock.Object.Send(command).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(result == string.Empty);
+            this.output.WriteLine(result);
+        }
     }
 }
